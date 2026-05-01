a700533597b2ba65ef146bc58cc8d6ccb280e8bf
somodip <matainja0144@gmail.com>
2026-05-01 19:52:00 +0530

    banner

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -24,10 +24,10 @@
         <picture>
             <!-- Mobile image -->
             <source media="(max-width: 768px)" 
-                    srcset="{{ asset('assets/images/mechanix-banner-mob.jpg') }}">
+                    srcset="{{ asset('assets/images/mechanix-banner-mob-02.jpg') }}">
 
             <!-- Desktop image -->
-            <img src="{{ asset('assets/images/mechanix-banner-01 1.jpg') }}" 
+            <img src="{{ asset('assets/images/mechanix-banner-desktop-02.jpg') }}" 
                  alt="Hero Image">
         </picture>
     </div>
