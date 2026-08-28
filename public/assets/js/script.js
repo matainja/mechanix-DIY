01550c8659ff7238be318bab0a2b324e74c101bc
somodip <matainja0144@gmail.com>
2026-08-28 18:52:59 +0530

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -605,3 +605,24 @@ $('#mxAuthModal').on('hidden.bs.modal', function () {
     // Default tab
     $('#guestTab').tab('show');
 });
+
+
+// csrf token mismatch then reload
+fetch(url, options)
+    .then(response => {
+        if (response.status === 419) {
+            window.location.reload();
+            return;
+        }
+
+        return response.json();
+    })
+    .then(data => {
+        if (!data) return;
+
+        // normal response handling
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
